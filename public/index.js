3f73b9b512007eb2433dc7748411c41ef1788150
moruzerinho6 <zerinho6@gmail.com>
2020-07-17 20:08:05 -0300

    Update to use in another pc.

--- public/index.js
+++ public/index.js
@@ -1,1 +1,9 @@
-console.log('local caralho.')
+const urlParams = new URLSearchParams(window.location.search)
+const myParam = urlParams.get('path')
+const preview = document.getElementById('imagePreview')
+const screenName = document.getElementById('screenName')
+const literalScreenName = myParam.split('/')[myParam.split('/').length - 1]
+
+preview.src = `${window.location.origin}/images/${myParam}/default.jpg`
+screenName.innerText = `Now looking at ${literalScreenName}`
+console.log(urlParams)
--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require('express')
-const fs = require('fs')
+// const fs = require('fs')
 const app = express()
 
 // our default array of dreams
@@ -12,7 +12,7 @@ const dreams = [
   'Find and count some sheep',
   'Climb a really tall mountain',
   'Wash the dishes'
-];
+]
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -20,15 +20,16 @@ app.use(express.static('public'))
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get('/', (request, response) => {
-  let endsWithDefault = false
-  let hasHtml = false
-
+  // let endsWithDefault = false
+  // let hasHtml = false
   // /?images=soundwaves/4.6.0/default.png&test=1
   console.log(request._parsedUrl)
+  /*
   if (Object.keys(request.query).length >= 1 && request.query.image) {
     endsWithDefault = typeof request.query.image === 'string' ? request.query.image.split('/')[request.query.image.split('/').length - 1].endsWith('default.jpg') : false
   }
   console.log(endsWithDefault)
+  */
   response.sendFile(`${__dirname}/public/step.html`)
   // response.sendFile(__dirname + `/public/images/${request.query.image}${!endsWithDefault ? `/default.jpg` : ''}`)
 })
